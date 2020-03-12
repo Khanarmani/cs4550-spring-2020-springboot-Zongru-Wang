9967c2f3c409b93d2b7741ffedd1b10072fde9a4
wangzongru <wang.zong@husky.neu.edu>
2020-03-12 12:03:09 -0400

    added widgettypes  enumic class and added widget order make widget model has fields as reqired

--- src/main/java/com/example/myapp/models/Widget.java
+++ src/main/java/com/example/myapp/models/Widget.java
@@ -10,47 +10,51 @@ import javax.persistence.*;
 @Entity
 @Table(name = "widgets")
 public class Widget {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String title = "New Widget";
-    private String type = "HEADING";
-    private String text = "New Widget";
-    private int size = 2;
+    private Integer id;
 
-    @ManyToOne
+    private String title;
+
+    // deafult type
+    private WidgetTypes type = WidgetTypes.HEADING;
+
     @JsonIgnore
+    @ManyToOne
     private Topic topic;
 
-    public Topic getTopic() {
-        return topic;
+
+
+
+    private Integer size;
+    private String text;
+
+    // Absolute or relative URL referring to online resource
+    private String src;
+
+    private Integer widgetOrder;
+
+
+    private Integer width;
+    private Integer height;
+    private String value;
+
+
+    public Widget(String title, WidgetTypes type) {
+        this.title = title;
+        this.type = type;
     }
 
-    public void setTopic(Topic topic) {
-        this.topic = topic;
+    public Widget() {
     }
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public int getId() {
+    // Getters and Setters
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -62,11 +66,75 @@ public class Widget {
         this.title = title;
     }
 
-    public String getType() {
+    public WidgetTypes getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(WidgetTypes type) {
         this.type = type;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Integer getwidgetOrder() {
+        return widgetOrder;
+    }
+
+    public void setwidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
---
+++ src/main/java/com/example/myapp/models/WidgetTypes.java
@@ -0,0 +1,11 @@
+package com.example.myapp.models;
+
+public enum WidgetTypes {
+    HEADING,
+    LIST,
+    PARAGRAPH,
+    IMAGE,
+    YOUTUBE,
+    HTML,
+    LINK
+}
